79c16eacb7f9670ac6ae659db630bd0c387af148
Gautam <gautampadhi55@gmail.com>
2023-09-24 02:36:58 +0530

    updates

--- TD CA/game_board.py
+++ TD CA/game_board.py
@@ -1,43 +1,85 @@
 import pygame
-import numpy as np
-import time
 
 class GameBoard:
-    def __init__(self, ca, cell_size = 10):
+    def __init__(self, ca, cell_size=10):
+        self.ca = ca
         self.width = ca.width
         self.height = ca.height
         self.cell_size = cell_size
-        
-        self.screen = pygame.display.set_mode((self.width*self.cell_size, 
-                                               self.height*self.cell_size))
-        pygame.display.set_caption("Cellular Automaton Simulation")
-        
-        self.clock = pygame.time.Clock()
-        self.grid_board = ca.grid
         self.running = False
-        
-        self.color_bg = (10,10,10)
-        self.color_grid = (60,60,60)
-        self.color_die_next = (180,180,180)
-        self.color_alive = (255,255,255)    
-        
-        
-    def toggle_simulation(self):
-        self.running = not self.running
-        
+        self.mouse_dragging = False
+
+        # Define colors
+        self.bg_color = (0, 0, 0)
+        self.grid_color = (128, 128, 128)
+        self.cell_color = (255, 255, 255)
+
+        self.screen = pygame.display.set_mode((self.width*cell_size, 
+                                               self.height*cell_size))
+        pygame.display.set_caption("Cellular Automaton")
+
+    def draw_grid(self):
+        for y in range(0, self.height * self.cell_size, self.cell_size):
+            pygame.draw.line(self.screen, self.grid_color, 
+                             (0, y), (self.width * self.cell_size, y))
+
+        for x in range(0, self.width * self.cell_size, self.cell_size):
+            pygame.draw.line(self.screen, self.grid_color, 
+                             (x, 0), (x, self.height * self.cell_size))
+
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                return
+                exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    self.toggle_simulation()
+                    self.running = not self.running
+
+        mouse_buttons = pygame.mouse.get_pressed()
+        if mouse_buttons[0]:
+            self.mouse_dragging = True  # Start dragging
+            self.update_cells_on_drag()  # Update cells while dragging
+        else:
+            self.mouse_dragging = False
             
-            
+    def update_cells_on_drag(self):
+        if self.mouse_dragging:
+            pos = pygame.mouse.get_pos()
+            col = pos[0] // self.cell_size
+            row = pos[1] // self.cell_size
+            self.ca.set_cell_state(row, col, 1)
+
+    def toggle_cell(self, pos):
+        x, y = pos
+        col = x // self.cell_size
+        row = y // self.cell_size
+        current_state = self.ca.get_cell_state(row, col)
+        new_state = 1 if current_state == 0 else 0
+        self.ca.set_cell_state(row, col, new_state)
+
+    def run_simulation(self):
+        clock = pygame.time.Clock()
+        while True:
+            self.handle_events()
             if self.running:
-                if pygame.key.get_pressed()[0]:
-                    pos = pygame.mouse.get_pos()
-                    col = pos[0]//self.cell_size
-                    row = pos[0]//self.cell_size
-            
+                self.ca.evolve()
+            self.screen.fill(self.bg_color)
+            self.draw_grid()
+            self.draw_cells()
+            pygame.display.flip()
+            clock.tick(10)
+
+    def draw_cells(self):
+        for y in range(self.ca.height):
+            for x in range(self.ca.width):
+                if self.ca.get_cell_state(y, x):
+                    pygame.draw.rect(
+                        self.screen,
+                        self.cell_color,
+                        pygame.Rect(x * self.cell_size, 
+                                    y * self.cell_size, 
+                                    self.cell_size, 
+                                    self.cell_size),
+                    )
--- TD CA/main_run.py
+++ TD CA/main_run.py
@@ -1,20 +1,14 @@
+import pygame 
+import time
+import numpy as np
 from CA import TwoDimensionalCA
 from game_board import GameBoard
-import pygame
+
 
 pygame.init()
 
-# Define the dimensions and rules for the cellular automaton
-width = 50
-height = 50
-birth_rule = ["3"]
-survival_rule = ["2", "3"]
+ca = TwoDimensionalCA(80,60,['3'],['2','3'])
 
-# Create a TwoDimensionalCA instance
-ca = TwoDimensionalCA(width, height, birth_rule, survival_rule)
+gb = GameBoard(ca)
 
-# Create a GameBoard instance with the CA
-pygame_sim = GameBoard(ca)
-
-# Run the Pygame simulation
-pygame_sim.run_simulation()
+gb.run_simulation()
